9262997c28180c8698ce3b37ea9d84c43a07858a
L Palmito <yannickmorel1@gmail.com>
2017-07-19 18:56:01 +0200

    First commit

---
+++ answers.py
@@ -0,0 +1,20 @@
+reponse_1 = "print(data)"
+
+reponse_2_1 = "A déterminer la couleur des points"
+reponse_2_2 = "A déterminer l'échelle des points"
+reponse_2_3 = "A déterminer la transparence des points"
+reponse_2_4 = "A déterminer la couleur des contours des points"
+reponse_2_5 = "A ajouter le titre 'Mon 1er graphe avec python'"
+reponse_2_6 = "A afficher la grille de fond"
+
+reponse_3 = "'nb_clusters' représente le nombre de clusters\n'init_means' représente les coordonées des centres initiaux\nLes paramètres les plus adaptés sont 2 et [[1,1],[1,3],[3,2]]"
+
+reponse_4 = "'init' représente les centres initiaux\n'n_clusters' le nombre de clusters utilisés\n'n_init' le nombre d'initialisations essayées"
+
+reponse_5 = "Ce sont des codes héxadécimaux codant les différentes couleurs utilisées pour le graphe."
+
+reponse_6 = "'k_means_cluster_centers' représente les coordonnées des centres des clusters\n'k_means_labels' représente l'appartenance des points à l'un des clusters\nPour le voir utilisez 'print' bien sûr ! ('print(k_means_cluster_centers)' et 'print(k_means_labels)')"
+
+reponse_7 = "Il permet de coder si le point est dans le cluster k ou non"
+
+reponse_8 = "A ajouter le titre 'Clusters via k-means'"
